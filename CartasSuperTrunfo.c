551aebb2d7f5ed7e7a76369e735838cdaf08756f
paulor99 <pr.deoliveirafilho@gmail.com>
2025-01-13 17:36:45 +0000

    programa completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,6 +18,35 @@ int main() {
     float pib;
     int pnt_turisticos;
 
+    //print e leitura de dados
+    printf("Insira o código da cidade: \n");
+    scanf("%d", &codigo_cidade);
+
+    printf("Insira o nome da cidade: \n");
+    scanf("%s", &nome_cidade);
+
+    printf("Insira a população da cidade: \n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área da cidade \n");
+    scanf("%f", &area);
+
+    printf("Insira o pib da cidade: \n");
+    scanf("%d", &pib);
+
+    printf("Insira o número de pontos turisticos: \n");
+    scanf("%d", &pnt_turisticos);
+
+    //Exibição dos dados
+    printf("Código da cidade: %d \n", codigo_cidade);
+    printf("Nome da cidade: %s \n", nome_cidade);
+    printf("População da cidade: %.3f \n", populacao);
+    printf("Área da cidade: %.2f \n", area);
+    printf("PIB da cidade: %f \n", pib);
+    printf("Número de pontos turísticos: %d \n", pnt_turisticos);
+
+
+
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
